eef62323bb2eb0f63abe9a635a393a05bd1341ab
Greg Turnquist <gturnquist@gopivotal.com>
2014-08-21 10:01:09 -0500

    Working on client

--- learning-spring-ws-client/src/main/java/com/greglturnquist/learningspringws/NetworkEventConfiguration.java
+++ learning-spring-ws-client/src/main/java/com/greglturnquist/learningspringws/NetworkEventConfiguration.java
@@ -4,9 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.client.support.interceptor.ClientInterceptor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
 import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
-import org.springframework.ws.soap.security.xwss.callback.SpringPlainTextPasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.xwss.callback.SpringUsernamePasswordCallbackHandler;
 
 @Configuration
 public class NetworkEventConfiguration {
@@ -19,22 +21,30 @@ public class NetworkEventConfiguration {
 	}
 
 	@Bean
-	NetworkEventClient client(Jaxb2Marshaller marshaller, XwsSecurityInterceptor securityInterceptor) {
+	NetworkEventClient client(Jaxb2Marshaller marshaller) {
 		NetworkEventClient networkEventClient = new NetworkEventClient();
 		networkEventClient.setDefaultUri("http://localhost:8080/ws");
 		networkEventClient.setMarshaller(marshaller);
 		networkEventClient.setUnmarshaller(marshaller);
-		networkEventClient.setInterceptors(new ClientInterceptor[]{securityInterceptor});
-		//networkEventClient.setInterceptors(new ClientInterceptor[]{securityInterceptor});
+//		networkEventClient.setInterceptors(new ClientInterceptor[]{securityInterceptor()});
 		return networkEventClient;
 	}
 
 	@Bean
 	XwsSecurityInterceptor securityInterceptor() {
 		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
-		securityInterceptor.setCallbackHandler(new SpringPlainTextPasswordValidationCallbackHandler());
+		securityInterceptor.setSecureRequest(true);
+		securityInterceptor.setValidateRequest(true);
+		securityInterceptor.setCallbackHandler(callbackHandler());
 		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
 		return securityInterceptor;
+	}
+
+	@Bean
+	SpringUsernamePasswordCallbackHandler callbackHandler() {
+		SecurityContextHolder.setContext(new SecurityContextImpl());
+		SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("user", "password"));
+		return new SpringUsernamePasswordCallbackHandler();
 	}
 
 }
